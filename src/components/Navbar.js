183a381d32dbf20c6be64a13d2b4f5929f47a1f1
Chris <orj.tsuk@gmail.com>
2022-01-25 08:37:18 -0500

    setting the navbar up, adding initial files

---
+++ src/components/Navbar.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+
+export default function Card(){
+    return(
+        <div className="navbar--container">
+            <img alt="globe" src="../images/globe.png"/><h5 className="navbar--text">my travel journal</h5>
+        </div>
+    )
+}
