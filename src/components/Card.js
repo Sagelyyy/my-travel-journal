e290ae0e86cd34398c7d2086c7aadbda39156cab
Chris <orj.tsuk@gmail.com>
2022-01-26 08:26:14 -0500

    working on styling and spacing for the card, and setting the text to display properly.

--- src/components/Card.js
+++ src/components/Card.js
@@ -5,15 +5,16 @@ export default function Card(props){
 
     return(
         <div className="card--container">
-            <img alt="vacation-spot" src={props.item.imageUrl} />
+            <img className="card--img" alt="vacation-spot" src={props.item.imageUrl} />
             <div className="card--info">
                 <div className="card--location">
-                <h3>{props.item.location}</h3> 
-                <h5> View on Google Maps</h5>
+                <img className="card--arrow" alt="google maps arrow" src="./images/maps-arrow.png" />
+                <h3 className="card--country">{props.item.location}</h3> 
+                <a className="card--link" href={`${props.item.googleMapsUrl}`}>View on Google Maps</a>
                 </div>
-                <h1>{props.item.title}</h1>
-                <h5>{props.item.startDate} - {props.item.endDate}</h5>
-                <h6>{props.item.description}</h6>
+                <h1 className="card--title">{props.item.title}</h1>
+                <h5 className="card--date">{props.item.startDate} - {props.item.endDate}</h5>
+                <h6 className="card--desc">{props.item.description}</h6>
             </div>
         </div>
     )
--- src/data.js
+++ src/data.js
@@ -6,7 +6,7 @@ export default [
         startDate: "12 Jan, 2021",
         endDate: "24 Jan, 2021",
         description: "Mount Fuji is the tallest mountain in Japan, standing at 3,776 meters (12,380 feet). Mount Fuji is the single most popular tourist site in Japan, for both Japanese and foreign tourists.",
-        imageUrl: "https://unsplash.com/photos/WLxQvbMyfas"
+        imageUrl: "./images/mt-fuji.jpg"
     },
     {
         title: "Seoul",
@@ -14,7 +14,7 @@ export default [
         googleMapsUrl: "https://goo.gl/maps/1DGM5WrWnATgkSNB8",
         startDate: "4 May, 2022",
         endDate: "11 May, 2022",
-        description: "Seoul is the Hears of South Korea, with food and nightlife that you can get lost in.",
-        imageUrl: "https://unsplash.com/photos/WLxQvbMyfas"
+        description: "Seoul  officially the Seoul Special City, is the capita; and largest metropolis of South Korea. Seoul has a population of 9.7 million people, and forms the heart of the Seoul Capital Area with the surrounding Incheon metropolis and Gyeonggi province. Considered to be a global city and rated as an Alpha – City, Seoul was the world's 4th largest metropolitan economy in 2014 after Tokyo, New York City and Los Angeles.",
+        imageUrl: "./images/seoul.jpeg"
     }
 ]
